e6d5e40be8826756d15fd4b82ceafe2efe972537
ahmalla <ahmalla@hotmail.com>
2022-11-16 16:50:56 -0800

    finished lab

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
 
 import './App.css'
-import NewSkillForm from './NewSkillForm';
 import SkillList from './SkillListItem';
+import { useState } from "react";
 
-const skills = [
-  { name: "HTML", level: 5 },
-  { name: "CSS", level: 3 },
-  { name: "JavaScript", level: 4 },
-  { name: "Python", level: 2 },
-];
-
-
-  
 
 function App() {
+  const [skills, setSkills] = useState ([
+    { name: "HTML", level: 5 },
+    { name: "CSS", level: 3 },
+    { name: "JavaScript", level: 4 },
+    { name: "Python", level: 2 },
+
+  ]);
+  const [newSkill, setNewSkills] = useState('')
+  const [newLevel, setNewLevel] = useState('')
+
+  const handleAddSkill = (event) => {
+    event.preventDefault()
+    setSkills([{name:newSkill, level:newLevel}, ...skills])
+  }
+  const handleSkillChange = (event) => {
+    setNewSkills(event.target.value)
+  }
+  const handleLevelChange = (event) => {
+  setNewLevel(event.target.value)
+  }
   return (
-    <div className="App">
+    <div>
       <h1 style={{fontSize: "48px", color: "teal"}}>React Dev Skills</h1>
       {skills.map(function (skill) {
         return <SkillList key={skill.name} name={skill.name} level={skill.level} />
-
 })}
-      <NewSkillForm />
+      <div className="App">
+        <form className="NewSkillForm" onSubmit={handleAddSkill}>
+          <label>Skill</label>
+          <input value={newSkill} onChange={handleSkillChange}></input>
+          <label>Level</label>
+          <select value={newLevel} onChange={handleLevelChange}>
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+          </select>
+          <button type='submit'>Add SKill</button>
+        </form>
+    </div>
     </div>
   )
 }
--- src/NewSkillForm.js
+++ src/NewSkillForm.js
@@ -1,21 +1,12 @@
 
 
-function NewSkillForm() {
-    return (
-    <div className="NewSkillForm">
-        <form>
-          <label>Skill</label><input className="NewSkillForm"></input><label>Level</label>
-          <select className="NewSkillForm">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </select>
-          <button className="NewSkillForm">Add SKill</button>
-        </form>
-    </div>
+// function NewSkillForm() {
+//   const handleAddSkill = () => {
+    
+//   }
+//     return (
+    
 
-)}
+// )}
 
-export default NewSkillForm
+// export default NewSkillForm
